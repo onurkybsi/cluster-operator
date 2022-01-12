136bacb36240d07ca0e80823c74929ae4b5259c6
onurkybsi <onurbpm@outlook.com>
2022-01-12 16:41:33 +0100

    First commit

---
+++ src/main/java/org/kyb/clusteroperator/ClusterOperatorApplication.java
@@ -0,0 +1,13 @@
+package org.kyb.clusteroperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kyb/clusteroperator/ClusterOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kyb.clusteroperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
