823527d30f77ee3e239bdfc44eb7033d298559fb
onurkybsi <onurbpm@outlook.com>
2022-01-20 09:37:39 +0100

    KUBECONFIG_FILE_PATH logging was added

--- src/main/java/org/kyb/clusteroperator/clusterClients/configurations/Fabric8KubernetesClientConfiguration.java
+++ src/main/java/org/kyb/clusteroperator/clusterClients/configurations/Fabric8KubernetesClientConfiguration.java
@@ -4,6 +4,8 @@ import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,14 +22,19 @@ public class Fabric8KubernetesClientConfiguration {
     @Value("${KUBECONFIG_FILE_PATH}")
     private String kubeconfigFilePath;
 
+    private Logger logger = LoggerFactory.getLogger(Fabric8KubernetesClientConfiguration.class);
+
     @Bean
     public KubernetesClient fabric8KubernetesClient()
             throws IOException {
+        logger.info("KUBECONFIG_FILE_PATH was received as: " + kubeconfigFilePath);
+
         Config config;
         if (kubeconfigFilePath != null)
             config = Config.fromKubeconfig(Files.readString(Paths.get(kubeconfigFilePath)));
         else
             config = new ConfigBuilder().build();
+
         return new DefaultKubernetesClient(config);
     }
 }
